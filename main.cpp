b68ca6dd35f9021fe25fdd6d39dfc19db385924e
meats88 <pcx883mi@gmail.com>
2026-03-24 18:52:43 -0700

    done

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@ using namespace std;
 int main()
 {
   int numFemale, numMale, numOthers;
-  double percF, percM, percO;
+  double percF, percM, percO, total;
 
-  cout << "Enter the number of students: Male, Female and Others";
+  cout << "Enter the number of students: Male, Female and Others:";
   cin >> numMale >> numFemale >> numOthers;
   // TODO
+  total = numMale + numFemale + numOthers;
+  percM = (numMale / total) * 100;
+  percF = (numFemale / total) * 100;
+  percO = (numOthers / total) * 100;
+
 
   cout << setprecision(2) << fixed;
   cout << "Percentage of Male: " << percM << endl;
